d8d8a40b9893b63179fd2c19a528a515d6768aa8
SpellMender <spellmender@gmail.com>
2023-04-16 03:54:43 -0600

    Referencing deltaTime in player movement to manually dilate time for player
    Jump still isn't working.

---
+++ Assets/Production/SpellMender/Buggy/Parallax.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Chrono
+{
+
+    //-----------------------------------------------------
+    // Source: Dani - How to infinite scrolling background
+    // Link: https://www.youtube.com/watch?v=zit45k6CUMk
+    //-----------------------------------------------------
+
+    public class Parallax : MonoBehaviour
+    {
+        float lengthX, startposX, lengthY, startposY;
+        public GameObject cam;
+        public float parallaxEffectX, parallaxEffectY;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            startposX = transform.position.x;
+            startposY = transform.position.y;
+
+            lengthX = GetComponent<SpriteRenderer>().bounds.size.x;
+            lengthY = GetComponent<SpriteRenderer>().bounds.size.y;
+        }
+
+        // Update is called once per frame
+        void LateUpdate()
+        {
+            float tempX = (cam.transform.position.x * (1 - parallaxEffectX));
+            //float tempY = (cam.transform.position.y * (1 - parallaxEffectY));
+
+            float distX = (cam.transform.position.x * parallaxEffectX);
+            //float distY = (cam.transform.position.y * parallaxEffectY);
+
+            //transform.position = new Vector3(startposX + distX, startposY + distY, transform.position.z);
+            transform.position = new Vector3(startposX + distX, transform.position.y, transform.position.z);
+
+            if (tempX > startposX + lengthX) startposX += lengthX;
+            else if (tempX < startposX - lengthX) startposX -= lengthX;
+        }
+    }
+
+}
---
+++ Assets/Production/SpellMender/Player.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    float speed, jumpMagnitude;
+
+    [SerializeField]
+    AudioClip jumpSound;
+
+    float moveDelta, jumpDelta, moveInput;
+    bool isMoving, isJumping, flipX;
+
+    Vector3 movement;
+    Vector3 jumpVector;
+
+    Rigidbody2D playerBody;
+    SpriteRenderer playerSprite;
+    Animator playerAnim;
+    AudioSource playerAudio;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerBody = GetComponent<Rigidbody2D>();
+        playerSprite = GetComponent<SpriteRenderer>();
+        playerAnim = GetComponentInChildren<Animator>();
+        playerAudio = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        ReadInput();
+        SetFlags();
+        Animate();
+    }
+    void FixedUpdate()
+    {
+        Move();
+        Jump();
+    }
+
+    void Move()
+    {
+        if (isMoving)
+        {
+            moveDelta = moveInput * speed * Time.deltaTime;
+            movement = transform.position + new Vector3(moveDelta, 0, 0);
+            playerBody.MovePosition(movement);
+        }
+    }
+
+    void Jump()
+    {
+        if (isJumping)
+        {
+            jumpDelta = jumpMagnitude * Time.deltaTime;
+            jumpVector = transform.position + new Vector3(0, jumpDelta, 0);
+            playerBody.MovePosition(jumpVector);
+
+            PlaySound(jumpSound);
+        }
+    }
+
+    bool IsGrounded()
+    {
+        float detectionRange = 0.05f;
+        BoxCollider2D collider = GetComponent<BoxCollider2D>();
+        RaycastHit2D groundDetection = Physics2D.BoxCast(collider.bounds.center, collider.bounds.size, 0f, Vector2.down, detectionRange, LayerMask.GetMask("Ground"));
+        return groundDetection.collider != null;
+    }
+
+    void ReadInput()
+    {
+        // Resolves input issue between scenes by reading horizontal input every frame on each specific key
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+        {
+            moveInput = Input.GetAxisRaw("Horizontal");
+        }
+        else if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow) || IsGrounded())
+        {
+            // consider optimizing the IsGrounded method check so it doesn't happen every frame
+            moveInput = 0;
+        }
+
+        if (Input.GetKey(KeyCode.Space) && IsGrounded() && isJumping == false)
+        {
+            isJumping = true;
+        }
+        else isJumping = false;
+    }
+
+    void SetFlags()
+    {
+        if (moveInput != 0)
+        {
+            // Set Moving Flag
+            if (!isMoving) isMoving = true;
+
+            // Set FlipX Flag
+            if (moveInput < 0 && !flipX) { flipX = true; }
+            else if (moveInput > 0 && flipX) { flipX = false; }
+        }
+        else if (isMoving) isMoving = false;
+
+        // Set Jumping Flag
+        if (!IsGrounded() && !isJumping) isJumping = true;
+        else if (isJumping && IsGrounded()) isJumping = false;
+    }
+
+    void Animate()
+    {
+        // Face the sprite using flipX flag
+        if (playerSprite.flipX != flipX) playerSprite.flipX = flipX;
+
+        // Set animation state
+        if (isJumping) playerAnim.Play("player_jump");
+        else if (isMoving) playerAnim.Play("player_run");
+        else playerAnim.Play("player_idle");
+    }
+
+    void PlaySound(AudioClip sound)
+    {
+        playerAudio.clip = sound;
+        playerAudio.Play();
+    }
+
+}
