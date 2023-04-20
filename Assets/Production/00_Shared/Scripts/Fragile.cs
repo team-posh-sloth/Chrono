0b3db62389f1f3910f00b6dadee745c39bba705d
SpellMender <spellmender@gmail.com>
2023-04-19 20:28:50 -0600

    Fragile script added to obstacle prefabs

---
+++ Assets/Production/00_Shared/Scripts/Fragile.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Chrono
+{
+    public class Fragile : MonoBehaviour
+    {
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.gameObject.layer == 8) Destroy(gameObject);
+        }
+    }
+}
